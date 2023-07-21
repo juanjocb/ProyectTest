42d382f981c66c7f49928de40ec0d477c40113df
Juan Jose Cañas <juanjo2003gg@gmail.com>
2023-07-21 00:00:06 -0500

    Proyect Finish

--- src/libs/objects-array.js
+++ src/libs/objects-array.js
@@ -10,6 +10,13 @@ Salida: [name, surname, food]
 //Dado un array de objetos, encontrar sus claves unicas.
 
 const uniqueKey = (array) => {
-    throw Error("Sin implementar")
-}
+    const arrayOfKeys = array.map((element) => Object.keys(element))
+    const uniqueKeys = [].concat(...arrayOfKeys)
+    const uniqueKeysSet = new Set(uniqueKeys)    
+    return Array.from(uniqueKeysSet);
+};
 
+
+module.exports = {
+  uniqueKey,
+};
--- test/objects-array.test.js
+++ test/objects-array.test.js
@@ -1,1 +1,55 @@
-test("Un test que no hace nada", () => {});
+const { uniqueKey } = require("../src/libs/objects-array");
+
+test("La funcion no explota", () => {
+  uniqueKey([]);
+});
+
+test("Si recibe un array vacio, devuelve un array vacio", () => {
+  const result = uniqueKey([]);
+  expect(result).toEqual([]);
+});
+
+test("Si recibe un objeto vacio, devuelve un objeto vacio", () => {
+  const result = uniqueKey([{}]);
+  expect(result).toEqual([]);
+});     
+
+test("Si recibe un objeto de una clave, devuelve un esa clave", () => {
+    const array = [{ name: "juan"}]
+    const result = uniqueKey(array);
+    expect(result).toEqual(["name"]);
+  });
+
+test("Si recibe un objeto con 3 claves, devuelve las claves", () => {
+    const array = [{ name: "juan", surname: "cañas", age: 19}]
+    const result = uniqueKey(array);
+    expect(result).toEqual(["name", "surname", "age"]);
+  });
+
+  
+test("Si recibe array con 2 objetos de 1 clave, devuelve las claves", () => {
+    const array = [{ name: "juan"}, {surname: "cañas"}, {age: 19}]
+    const result = uniqueKey(array);
+    expect(result).toEqual(["name", "surname", "age"]);
+  });
+
+  test("Si recibe array con 2 objetos con la misma clave, devuelve 1 sola clave", () => {
+    const array = [{ name: "juan"}, {name: "cañas"}, {age: 19}]
+    const result = uniqueKey(array);
+    expect(result).toEqual(["name", "age"]);
+  });
+
+  test("Si recibe array con muchos objetos con claves diferentes e iguales, devuelve las claves", () => {
+    const array = 
+    [
+        { name: "juan", surname:"25", food: 19}, 
+        { name: "cañas", city: "Armenia"}, 
+        { age: 19}, 
+        { sport: "soccer", surname:"fabricio", age: 19}, 
+        { food: "pizza", surname:"giraldo", age: 19}, 
+        { age: 19}
+    ]
+    const result = uniqueKey(array);
+    expect(result).toEqual(["name", "surname", "food", "city", "age", "sport"]);
+  });
+
